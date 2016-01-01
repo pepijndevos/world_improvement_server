e1d9d9fe29118f380e2192e7fd93924b2a711ee3
Pepijn de Vos <pepijndevos@gmail.com>
2016-01-01 18:22:20 +0100

    copy old motd

--- rwall_server.c
+++ rwall_server.c
@@ -2,10 +2,14 @@
 #include "stdio.h"
 
 void * wallproc_wall_1_svc(wrapstring *s, struct svc_req *rqstp __unused) {
-	static void		*dummy = NULL;
+	static void *dummy = NULL;
+  static int msgno = 1;
 
+  char newfile[200];
+  sprintf(newfile, "motd%d.txt", msgno++);
+  remove(newfile);
+  rename("motd.txt", newfile);
   FILE *pfp;
-
   pfp = fopen("motd.txt", "w");
   if (pfp != NULL) {
     fprintf(pfp, "%s\n", *s);
